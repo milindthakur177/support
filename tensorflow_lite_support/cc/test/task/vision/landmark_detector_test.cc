2e54b5afc0a953ac09a07ff5dcf970b2e066c9c5
Milind Thakur <milind170799@gmail.com>
2021-08-25 00:47:18 +0530

    Update landmark_detector_test.cc

--- tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
+++ tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
@@ -114,9 +114,8 @@ TEST_F(CreateFromOptionsTest, FailsWithMissingModel) {
 
   EXPECT_EQ(landmark_detector_or.status().code(),
             absl::StatusCode::kInvalidArgument);
-  EXPECT_THAT(landmark_detector_or.status().GetPayload(kTfLiteSupportPayload),
-              Optional(absl::Cord(
-                  absl::StrCat(TfLiteSupportStatus::kInvalidArgumentError))));
+  EXPECT_THAT(landmark_detector_or.status().message(),
+              HasSubstr("Missing mandatory `model_file` field in `base_options`"));
 }
 
 
