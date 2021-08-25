a999ae8165daf69378b799446dd05b78844da3b6
Milind Thakur <milind170799@gmail.com>
2021-08-25 11:26:33 +0530

    d

--- tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
+++ tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
@@ -150,8 +150,8 @@ TEST_F(DetectTest, SucceedsWithFloatModel) {
   const LandmarkResult& result = result_or.value();
 
   for (int i =0 ; i<num_keypoints ; ++i){
-    EXPECT_NEAR(result.landmarks(i).position(0), GOLDEN_KEY_Y[i], 0.025);
-    EXPECT_NEAR(result.landmarks(i).position(1), GOLDEN_KEY_X[i], 0.025);
+    EXPECT_NEAR(result.landmarks(i).position(), GOLDEN_POSITION[i], 0.025);
+    //EXPECT_NEAR(result.landmarks(i).position(1), GOLDEN_KEY_X[i], 0.025);
     EXPECT_NEAR(result.landmarks(i).score(), GOLDEN_SCORE[i], 0.52);
     
   }
