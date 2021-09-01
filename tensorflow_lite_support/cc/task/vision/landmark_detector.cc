84c29ec1419f181b6c1773981314a2f51752a12f
Milind Thakur <milind170799@gmail.com>
2021-09-01 11:04:13 +0530

    Update landmark_detector.cc

--- tensorflow_lite_support/cc/task/vision/landmark_detector.cc
+++ tensorflow_lite_support/cc/task/vision/landmark_detector.cc
@@ -57,16 +57,10 @@ StatusOr<std::unique_ptr<LandmarkDetector>> LandmarkDetector::CreateFromOptions(
 
   std::unique_ptr<LandmarkDetector> landmark_detector;
   if (options_copy->base_options().has_model_file()) {
-    ASSIGN_OR_RETURN(landmark_detector,
+    ASSIGN_OR_RETURN(auto landmark_detector,
                      TaskAPIFactory::CreateFromBaseOptions<LandmarkDetector>(
                          &options_copy->base_options()));
-  } else {
-    // Should never happen because of SanityCheckOptions.
-    return CreateStatusWithPayload(
-        StatusCode::kInvalidArgument,
-        absl::StrFormat("Missing mandatory `model_file` field in `base_options`"),
-        TfLiteSupportStatus::kInvalidArgumentError);
-  }
+  } 
   RETURN_IF_ERROR(landmark_detector->Init(std::move(options_copy)));
 
   return landmark_detector;
