c1243711aa55f6b012a51be33876793500c28288
Milind Thakur <milind170799@gmail.com>
2021-09-01 22:45:17 +0530

    Update landmark_detector_test.cc

--- tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
+++ tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
@@ -20,8 +20,6 @@ limitations under the License.
 #include "absl/flags/flag.h"
 #include "absl/status/status.h"
 #include "tensorflow/lite/core/shims/cc/shims_test_util.h"
-#include "tensorflow/lite/mutable_op_resolver.h"
-#include "tensorflow_lite_support/cc/common.h"
 #include "tensorflow_lite_support/cc/port/gmock.h"
 #include "tensorflow_lite_support/cc/port/gtest.h"
 #include "tensorflow_lite_support/cc/port/status_macros.h"
@@ -64,21 +62,21 @@ constexpr char kMobileNetFloatModel[] =
 
 // List of expected y coordinates of each keypoint
 constexpr float GOLDEN_KEY_Y[] = {
-    0.31545776, 0.29907033, 0.3031672,  0.3031672,  0.30726406, 0.3482326,
-    0.4096854,  0.30726406, 0.4260728,  0.2581018,  0.4260728,  0.49162248,
-    0.5530753,  0.34413573, 0.73333687, 0.27858606, 0.9299859};
+    0.31545776, 0.29907033, 0.3031672, 0.3031672, 0.30726406, 0.3482326,
+    0.4096854, 0.30726406, 0.4260728, 0.2581018, 0.4260728, 0.49162248,
+    0.5530753, 0.34413573, 0.73333687, 0.27858606, 0.9299859};
 
 // List of expected x coordinates of each keypoint
 constexpr float GOLDEN_KEY_X[] = {
-    0.4260728,  0.44246024, 0.44655707, 0.48752564, 0.47523507, 0.589947,
-    0.48342878, 0.72514313, 0.34413573, 0.8357582,  0.24581124, 0.73743373,
-    0.6841746,  0.88492055, 0.7210463,  0.8644362,  0.7128526};
+    0.4260728, 0.44246024, 0.44655707, 0.48752564, 0.47523507, 0.589947,
+    0.48342878, 0.72514313, 0.34413573, 0.8357582, 0.24581124, 0.73743373,
+    0.6841746, 0.88492055, 0.7210463, 0.8644362, 0.7128526};
 
 // List of expected scores of each keypoint
-constexpr float GOLDEN_SCORE[] = {
-    0.70056206, 0.6350124, 0.24581124, 0.8808236,  0.75382113, 0.75382113,
-    0.90540475, 0.925889,  0.8808236,  0.75382113, 0.8029834,  0.8029834,
-    0.84395194, 0.8029834, 0.96685755, 0.6350124,  0.9422764};
+constexpr float GOLDEN_SCORE[] = { 
+		0.700562, 0.880824, 0.753821, 0.569463, 0.43017, 0.753821, 0.802983, 
+		0.925889, 0.942276, 0.700562, 0.802983, 0.802983, 0.905405, 0.635012, 
+		0.905405, 0.700562, 0.905405};
 
 StatusOr<ImageData> LoadImage(std::string image_name) {
   return DecodeImageFromFile(
@@ -124,7 +122,7 @@ TEST_F(DetectTest, SucceedsWithFloatModel) {
   for (int i = 0; i < num_keypoints; ++i) {
     EXPECT_NEAR(result.landmarks(i).position(0), GOLDEN_KEY_Y[i], 0.025);
     EXPECT_NEAR(result.landmarks(i).position(1), GOLDEN_KEY_X[i], 0.025);
-    EXPECT_NEAR(result.landmarks(i).score(), GOLDEN_SCORE[i], 0.51);
+    EXPECT_NEAR(result.landmarks(i).score(), GOLDEN_SCORE[i], 0.025);
   }
 }
 
